e22639b7059256624925f9e1d39b5dad6d5e5f96
neal1991 <neal1991@sina.com>
2017-03-29 18:54:45 +0800

    add header

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,16 @@
 //index.js
 //获取应用实例
 var app = getApp()
-console.log('app globalData.name: ' + app.globalData.name)
 Page({
   data: {
-    motto: 'Hello World11111111',
-    userInfo: {
-      nickName:'Neal Caffery',
-      name: '123'
-      // age: 10,
-    },
-    buttonText: 'button'
+    array: ['中文', '英语'],
+    objectArray: [
+      {
+        id: 0,
+        name: '中文'
+      },
+    ],
+    index: 0,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -25,8 +25,6 @@ Page({
 
   },
   onLoad: function () {
-    console.log('onLoad')
-    console.log('why i not get userInfo')
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
@@ -39,6 +37,5 @@ Page({
   },
   onShow: function () {
     console.log('I have showed')
-    alert('can i alert')
   }
 })
